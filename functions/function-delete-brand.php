88d916a28dce7d2eac51583ce3aacef0c8e1862d
Chase Replogle <Chase Replogle>
2015-04-08 10:58:36 -0500

    Delete Brand

---
+++ functions/function-delete-brand.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Handles the Archiving of Brands
+ *
+ *
+ */
+
+
+// Prepopulates the Gravity Form Archive Form with the blog_ID
+add_filter('gform_field_value_brand_id', 'archive_brand_id');
+function archive_brand_id($value){
+    return get_current_blog_id();
+}
+
+
+// On Submission of Gravity Form
+add_action( 'gform_after_submission_6', 'after_archive_submission', 10, 2 );
+
+    function after_archive_submission($entry, $form){
+
+    	// Get the Brand ID from the gravity form
+    	$brand_ID = rgar( $entry, 2);
+
+    	// Set archive variable to True
+    	$deleted = 'Deleted';
+
+    	//Perform the Archive Function
+    	update_archived( $brand_ID, $deleted );
+
+    	//Get main site URL
+    	$main_site = get_site_url(1);
+
+    	// Redirect to main site dashboard after archive function
+    	wp_redirect( $main_site . "/dashboard"); exit;
+}
