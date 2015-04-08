5faae1bfb9234282c6b9ba96ed59f42d893e1409
Chase Replogle <Chase Replogle>
2015-04-08 11:15:56 -0500

    Delete Brand

--- functions/function-delete-brand.php
+++ functions/function-delete-brand.php
@@ -7,26 +7,29 @@
 
 
 // Prepopulates the Gravity Form Archive Form with the blog_ID
-add_filter('gform_field_value_brand_id', 'archive_brand_id');
-function archive_brand_id($value){
+add_filter('gform_field_value_brand_id', 'delete_brand_id');
+function delete_brand_id($value){
     return get_current_blog_id();
 }
 
 
 // On Submission of Gravity Form
-add_action( 'gform_after_submission_6', 'after_archive_submission', 10, 2 );
+add_action( 'gform_after_submission_6', 'after_delete_submission', 10, 2 );
 
-    function after_archive_submission($entry, $form){
+    function after_delete_submission($entry, $form){
 
-    	// Get the Brand ID from the gravity form
-    	$brand_ID = rgar( $entry, 2);
+        // Get the Brand ID from the gravity form
+        $brand_ID = rgar( $entry, 2);
 
-    	//Perform the Archive Function
-    	wpmu_delete_blog( $brand_ID, $drop );
+        // Set archive variable to True
+        $deleted = 3;
 
-    	//Get main site URL
-    	$main_site = get_site_url(1);
+        //Perform the Delete Function
+        update_archived( $brand_ID, $deleted );
 
-    	// Redirect to main site dashboard after archive function
-    	wp_redirect( $main_site . "/dashboard"); exit;
+        //Get main site URL
+        $main_site = get_site_url(1);
+
+        // Redirect to main site dashboard after archive function
+        wp_redirect( $main_site . "/dashboard"); exit;
 }
