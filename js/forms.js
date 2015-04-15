7d20989c6742e26cd3892e8fb67b1df227d84ec7
Chase Replogle <Chase Replogle>
2015-04-15 12:01:46 -0500

    Color Input

--- js/forms.js
+++ js/forms.js
@@ -30,6 +30,17 @@ $( document ).ready(function() {
 });
 
 
+$( document ).ready(function() {
+	$('li.color_picker').click(function(e) {
+	   e.preventDefault();
+
+	   $('.color_picker_field input').focus()
+
+	   return false;
+	})
+});
+
+
 
 
 
