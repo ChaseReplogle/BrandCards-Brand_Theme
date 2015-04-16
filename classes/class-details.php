7a7259d00f8af30e84352d758010f3c5442b7279
Chase Replogle <Chase Replogle>
2015-04-16 09:54:39 -0500

    Brand Header

--- classes/class-details.php
+++ classes/class-details.php
@@ -94,7 +94,7 @@ function brand_cover() {  ?>
 					<?php } else { ?>
 						<div class="brand-cover brand-cover-logo" style="background-color: #fff; border: 1px solid #dedede;">
 							<div class="card-inner">
-								<img id="image" src="<?php network_site_url(); ?>/wp-content/themes/brandcards/images/gray.svg"  class="card-image" />
+								<img id="image" src="<?php network_site_url(); ?>/wp-content/themes/brandcards/images/white.png"  class="card-image" />
 							</div>
 						</div>
 					<?php } ?>
