adbdfe5d7907435fda35853c2b6f9f98282caf1a
Chase Replogle <Chase Replogle>
2015-04-16 10:18:11 -0500

    Brand Header

--- classes/class-details.php
+++ classes/class-details.php
@@ -140,25 +140,27 @@ function brand_header() {
 					brand_cover(); ?>
 
 				</div>
-				<div class="col span_15 brand-dash-header_text <?php if ( is_front_page() ) { echo 'brand-dash-header_text-main'; } ?>">
 
-					<?php // Get Blog Details and display the brands name
-					$blog_details = get_blog_details(); ?>
-					<h1><?php echo $blog_details->blogname; ?> </h1>
 
-					<?php $args = array( 'posts_per_page' => 1, 'post_type' => 'brand_details' );
-					$details = get_posts( $args );
-					foreach ( $details as $detail ) :
-						$creator = get_post_meta($detail->ID, 'brand_creator', true);
-						$creator_url = get_post_meta($detail->ID, 'creator_website', true);
+				<?php // Get Blog Details and display the brands name
+				$blog_details = get_blog_details(); ?>
+				<h1><?php echo $blog_details->blogname; ?> </h1>
 
-						if($creator) { ?>
+				<?php $args = array( 'posts_per_page' => 1, 'post_type' => 'brand_details' );
+				$details = get_posts( $args );
+				foreach ( $details as $detail ) :
+					$creator = get_post_meta($detail->ID, 'brand_creator', true);
+					$creator_url = get_post_meta($detail->ID, 'creator_website', true);
+
+					if($creator) { ?>
+						<div class="col span_15 brand-dash-header_text <?php if ( is_front_page() ) { echo 'brand-dash-header_text-main'; } ?> <?php if($creator) { echo ' includes-creator' }">
 							<p class="secondary">Created by: <a href="<?php echo $creator_url ?>" class="secondary"><?php echo $creator ?></a></p>
-						<?php  }
+						</div>
+					<?php  }
 
-					endforeach; ?>
+				endforeach; ?>
 
-				</div>
+
 
 				<?php // Based on a users role edit links for the brand are displayed ?>
 
