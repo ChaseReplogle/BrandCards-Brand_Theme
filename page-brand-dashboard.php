cb7c7126f5242d68586f3700cd0c7b59797ebc94
Chase Replogle <Chase Replogle>
2015-05-06 09:46:27 -0500

    Update

--- page-brand-dashboard.php
+++ page-brand-dashboard.php
@@ -57,11 +57,9 @@ get_header(); ?>
 							while ( $cards->have_posts() ) {
 								$cards->the_post();
 								$postid = get_the_ID(); ?>
-									<div class="card-wrapper">
 										<a href="/?p=<?php echo $postid; ?>" class="card-link-a">
 											<?php card(); ?>
 										</a>
-									</div>
 							<?php }
 						} else {
 						}
